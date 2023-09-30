fc9088328880799bba34701e7f5ea10af9527a7e
Sergio Venegas <69369902+Angcroft@users.noreply.github.com>
2023-09-29 23:54:49 -0300

    Create blocks.py

---
+++ section_04/blocks.py
@@ -0,0 +1,5 @@
+for i in range(1, 13):
+
+    #   It is not necessary that the replacement fields should have numeration on them
+    print("No. {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
+    print("*" * 80)
