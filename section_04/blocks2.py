7963cbd1fbeb7b41805a44e364676775fadf5e02
Sergio Venegas <69369902+Angcroft@users.noreply.github.com>
2023-09-30 00:12:29 -0300

    Add files via upload

--- section_04/blocks2.py
+++ section_04/blocks2.py
@@ -6,5 +6,7 @@ print(age)
 
 if age >= 18:
     print("You are old enough to vote!")
+elif age == 900:
+    print("Sorry, Yoda, you die in Return of the Jedi")
 else:
     print("Please come back in {0} years".format(18 - age))
