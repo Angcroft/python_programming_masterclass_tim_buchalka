241953e6826d51d0532b1a314b916773225aff6e
Sergio Venegas <69369902+Angcroft@users.noreply.github.com>
2023-10-01 12:42:01 -0300

    Update strings2.py

--- section_04/strings2.py
+++ section_04/strings2.py
@@ -1,11 +1,13 @@
 number = "9,223;372,036 854,775;807"
 separators = ""
 
+#    This loop extracts the characters that don't belong in the number group to a string
 for char in number:
     if not char.isnumeric():
         separators = separators + char
 
 print(separators)
 
+#    These lines extract the numbers contained in the number variable, and stores them into an array
 values = "".join(char if char not in separators else " " for char in number).split()
 print([int(val) for val in values])
